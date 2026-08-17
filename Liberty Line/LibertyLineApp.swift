35ba75fcd441890426f4202a8a13cb6f66d82749
John Cleveland <john@zippymeals.com>
2026-08-17 00:56:33 -0400

    Made it so that the game only runs in landscape mode.

--- Liberty Line/LibertyLineApp.swift
+++ Liberty Line/LibertyLineApp.swift
@@ -1,12 +1,41 @@
 import SwiftUI
+import UIKit
 
 @main
 struct LibertyLineApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             RootView()
                 .statusBarHidden(true)
                 .persistentSystemOverlays(.hidden)
         }
+    }
+}
+
+@MainActor
+final class OrientationLock {
+    static let shared = OrientationLock()
+
+    private(set) var allowed: UIInterfaceOrientationMask = .landscape
+
+    func allow(_ mask: UIInterfaceOrientationMask) {
+        guard mask != allowed else { return }
+        allowed = mask
+        for scene in UIApplication.shared.connectedScenes {
+            guard let windowScene = scene as? UIWindowScene else { continue }
+            windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: mask))
+            for window in windowScene.windows {
+                window.rootViewController?.setNeedsUpdateOfSupportedInterfaceOrientations()
+            }
+        }
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        MainActor.assumeIsolated { OrientationLock.shared.allowed }
     }
 }
